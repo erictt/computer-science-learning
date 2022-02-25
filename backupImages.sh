b2b531f0886c53f8aea4537a61d04453d789d5c3
Eric Yang <ericyang879@gmail.com>
2022-02-25 13:54:38 -0500

    backup the images

---
+++ backupImages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+images=$(\grep -oh "https:\/\/i\.imgur\.com\/.*\.[jpgnif]*" -rn ./notes/ | sed "s/[0-9]*\:https\:\/\/i\.\imgur\.com\///g" | sort | uniq)
+downloaded=$(find ./images/ -name "*.[jpgpnif]*" | sed "s/.*\/\///g" | sort | uniq)
+prefix="https://i.imgur.com/"
+
+count=0
+skip=0
+
+for i in $images; do
+  exist=$(echo $downloaded | grep $i)
+  if [[ "$exist" == "" ]]; then
+    echo "$count: wget $prefix$i -O ./images/$i"
+    # wget $prefix$i -O ./images/$i
+    count=$((count+1))
+  else
+    # echo "skip: $i"
+    skip=$((skip+1))
+  fi
+done
+
+echo "Total skipped: $skip, total downloaded: $count"
